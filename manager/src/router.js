dcf807f159de97cee5e45c74c363a0c308e28b74
njx <1043006908@qq.com>
2019-06-11 09:09:56 +0800

    njx

---
+++ manager/src/router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Router, Route, Switch } from 'dva/router';
+// import IndexPage from './views/login/IndexPage';
+import HomePage from './views/home/IndexPage';
+
+function RouterConfig({ history }) {
+  return (
+    <Router history={history}>
+      <Switch>
+        {/* <Route path="/" exact component={IndexPage} /> */}
+        <Route path="/home" exact component={HomePage} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default RouterConfig;
