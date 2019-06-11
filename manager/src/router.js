0f735d07b8d9effc940b2509f43da29d2272ae94
Barbie <QQ@1070974945.com>
2019-06-11 21:59:18 +0800

    userlogin

--- manager/src/router.js
+++ manager/src/router.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 // import HomePage from './views/home/IndexPage';
 // import IndexPage from './views/login/IndexPage';
-import LoginPage from './views/Logins/index';
+import LoginPage from './views/login/index';
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
