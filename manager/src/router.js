13f7b8cdf9253f79bdc19f178e7ca51ef248cf7b
njx <1043006908@qq.com>
2019-06-14 12:00:44 +0800

    search成功

--- manager/src/router.js
+++ manager/src/router.js
@@ -3,7 +3,7 @@ import { Router, Route, Switch } from 'dva/router';
 // import IndexPage from "./views/IndexPage"
 import LoginPage from "@/views/login/Index"
 import HomePage from '@/views/home/Index';
-import ViewItemPage from "@views/Questions/dialog"
+// import ViewItemPage from "@views/Questions/dialog"
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
@@ -11,7 +11,7 @@ function RouterConfig({ history }) {
         {/* <Route path="/"  component={IndexPage} /> */}
         <Route path="/login" component={LoginPage} />
         <Route path="/" component={HomePage} />
-        <Route path="/questions/dialog" component={ViewItemPage} />
+        {/* <Route path="/questions/dialog" component={ViewItemPage} /> */}
       </Switch>
     </Router>
   );
