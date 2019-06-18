8c264db0df113dd180819a31666dd496ab67dd28
njx <1043006908@qq.com>
2019-06-19 00:34:54 +0800

    commit message

--- manager/src/views/login/index.js
+++ manager/src/views/login/index.js
@@ -1,97 +1,144 @@
-import React, {useEffect} from 'react';
-import { Form, Icon, Input, Button, Checkbox, message } from 'antd';
+import React,{useState,useEffect} from 'react';
 import { connect } from 'dva';
-import styles from './index.scss';
-  function LoginPage(props){  
-    useEffect(()=>{
-      if (props.isLogin === 1){
-        // 1.提示登陆成功
-        // 2.存储cookie
-        // 3.跳转主页面
-        message.success('登陆成功');
-        let pathName = decodeURIComponent(props.history.location.search.split('=')[1]);
-        props.history.replace(pathName||"/");
-      }else if(props.isLogin === -1){
-        // 登陆失败
-        message.error('用户名或密码错误')
-      }
-    }, [props.isLogin]);
-    let handleSubmit = e => {
-          e.preventDefault();
-          props.form.validateFields((err, values) => {
-            if (!err) {
-              let {login}=props;
-              login({
-                  user_name:values.username,
-                  user_pwd:values.password
-              })
-            }
-          });
-        };
-    //表单验证
-    const { getFieldDecorator } = props.form;
-     return  <div className="box">
-      <Form onSubmit={handleSubmit} className="login-form">
-        <Form.Item>
-            {getFieldDecorator('username', {
-              validateTrigger:"onBlur",
-              rules: [{ required: true,pattern:/^[a-zA-Z]+$/, message: '请输入正确的用户名!' }],
-            })(
-              <Input
-                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                placeholder="Username"
-              />,
-            )}
-        </Form.Item>
-        <Form.Item>
-            {getFieldDecorator('password', {
-              validateTrigger:"onBlur",
-              rules: [{ required: true,pattern:/^(?![0-9]+$)(?![a-z]+$)(?![A-Z]+$)(?!([^(0-9a-zA-Z)])+$)/, message: '请输入正确的密码!' }],
-            })(
-              <Input
-                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type="password"
-                placeholder="Password"
-              />,
-            )}
-        </Form.Item>
-        <Form.Item>
-            {getFieldDecorator('remember', {
-              valuePropName: 'checked',
-              initialValue: true,
-            })(<Checkbox>记住密码</Checkbox>)}
-            <a className="login-form-forgot" href="">
-            忘记密码
-            </a>
-            <Button type="primary" htmlType="submit" className="login-form-button">
-              登录
-            </Button>
-        </Form.Item>
-      </Form>
-  </div> 
-}
-  //props的类型检查
-  localStorage.propTypes={
+import styles from './Index.scss';
+import "antd/dist/antd.css";
+import { Link } from 'dva/router';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
 
-  }
-  //props的默认值
-  LoginPage.defaultProps={
-      
-  }
-  const mapStateToProps=state=>{
-      // console.log("state",state)
-      return{
-        ...state.user
+function LoginPage(props){
+  //获取login
+  let {login}=props;
+  useEffect(()=>{
+     login({
+       user_name:'chenmanjie',
+       user_pwd:'Chenmanjie123!'
+     })
+  });
+  //处理表单提交
+  let handleSubmit = e => {
+    e.preventDefault();
+    props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+        login({
+          user_name:values.username,
+          user_pwd:values.password
+        })
       }
-  }
-  const mapDispatchToProps=dispatch=>{
-      return{
-          login(payload){
-              dispatch({
-                  type:"user/login",
-                  payload
-              })
-          }
-      }
-  }
-export default connect(mapStateToProps,mapDispatchToProps)(Form.create()(LoginPage))
+    })
+  };
+  //表单校验
+  const { getFieldDecorator } = props.form;
+  return (
+    <Form onSubmit={handleSubmit} className={styles.login_form}>
+  <Form.Item>
+    {getFieldDecorator('username', {
+      rules: [{ required: true, message: '请输入用户名' }],
+    })(
+      <Input
+        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+        placeholder="请输入用户名"
+      />,
+    )}
+  </Form.Item>
+  <Form.Item>
+    {getFieldDecorator('password', {
+      rules: [{ required: true,
+      pattern:/^(?![0-9]+$)(?![a-z]+$)(?![A-Z]+$)(?!([^(0-9a-zA-Z)])+$)^.{8,16}$/,//进行验证大小写字母数字
+       message: '请输入正确格式的代码' }],
+    })(
+      <Input
+        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+        type="password"
+        placeholder="请输入用户名密码！"
+      />,
+    )}
+  </Form.Item>
+  <Form.Item>
+    {getFieldDecorator('remember', {
+      valuePropName: 'checked',
+      initialValue: true,
+    })(<Checkbox>记住密码</Checkbox>)}
+    <a className={styles.login_form_forgot} href="">
+      忘记密码
+    </a>
+    <Button type="primary" htmlType="submit" className={styles.login_form_button}>
+    <Link to="/home">登陆</Link>
+    </Button>
+  </Form.Item>
+</Form>
+  );
+}
+//props的类型检查
+LoginPage.propTypes={
+
+}
+//props的默认值
+LoginPage.defaultProps={
+
+}
+ const mapStateToProps=state=>{
+   return {}
+ }
+ const mapDispatchToProps=dispatch=>{
+   return {
+     login(payload){
+       dispatch({
+         type:'user/login',
+         payload//models里面的payload
+       })
+     }
+   }
+ }
+export default connect(mapStateToProps,mapDispatchToProps)(Form.create({name:'normal_login'})(LoginPage));
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+// function IndexPage() {
+//   function ChangeValue(e){
+//     console.log(e)
+//   }
+//   return (
+//    <div className={styles.box}>
+//       <Form.Item>
+//             <Input
+//             onChange={ChangeValue}
+//               className={styles.ipts}
+//               prefix={<Icon type="user" style={{ color: 'rgba(88,88,88)' }} />}
+//               placeholder="请输入用户名"
+//             />
+//         </Form.Item>
+//         <Form.Item>
+//             <Input
+//               className={styles.ipts}
+//               prefix={<Icon type="lock" style={{ color: 'rgba(88,88,88)' }} />}
+//               type="password"
+//               placeholder="请输入密码"
+//             />
+//         </Form.Item>
+//         <Form.Item>
+//           <div className={styles.lefts}>
+//           <Checkbox>记住密码</Checkbox>
+//           <a className="login-form-forgot" href="">
+//             忘记密码
+//           </a>
+//           </div>
+//           <Button type="primary" htmlType="submit" className={styles.loginBtn}>
+//             <Link to="/home">登录</Link>
+//           </Button>
+       
+//         </Form.Item>
+//    </div>
+//   );
+// }
+
+// IndexPage.propTypes = {
+// };
+
+// export default connect()(IndexPage);
