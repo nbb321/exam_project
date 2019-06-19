c1278416b20167f335094cd16d3edec06c73d79a
njx <1043006908@qq.com>
2019-06-19 23:51:17 +0800

    更改

--- manager/src/router.js
+++ manager/src/router.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-<<<<<<< HEAD
-// import IndexPage from './views/login/IndexPage';
-import HomePage from './views/home/IndexPage';
-
-// import HomePage from './views/home/IndexPage';
-import IndexPage from './views/login/IndexPage';
-=======
 import LoginPage from '@/views/login/index';
 import IndexPage from '@/views/Main/index';
 
->>>>>>> mc
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-<<<<<<< HEAD
-        {/* <Route path="/" exact component={IndexPage} /> */}
-        <Route path="/home" exact component={HomePage} />
-        <Route path="/" exact component={IndexPage} />
-        {/* <Route path="/home" exact component={HomePage} /> */}
-=======
         <Route path="/login" component={LoginPage} />
         <Route path="/" component={IndexPage} />
->>>>>>> mc
       </Switch>
     </Router>
   );
