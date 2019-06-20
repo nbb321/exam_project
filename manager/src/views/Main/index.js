f8681bc2624ed05036f8fa89cafb8a5d2829556b
Barbie <QQ@1070974945.com>
2019-06-20 17:40:24 +0800

    修改bug

--- manager/src/views/Main/index.js
+++ manager/src/views/Main/index.js
@@ -31,11 +31,11 @@ function IndexPage(props){
     <div>
       <img  className={styles.img} src="https://timgsa.baidu.com/timg?image&amp;quality=80&amp;size=b9999_10000&amp;sec=1551624718911&amp;di=4a7004f8d71bd8da84d4eadf1b59e689&amp;imgtype=0&amp;src=http%3A%2F%2Fimg105.job1001.com%2Fupload%2Falbum%2F2014-10-15%2F1413365052_95IE3msH.jpg" alt="" />
       <Dropdown overlay={
-          <Menu>
-          <Menu.Item  onClick={()=>props.changeLocal(props.locale==="zh"?"en":"zh")}>
+          <Menu onClick={()=>props.changeLocal(props.locale==="zh"?"en":"zh")}>
+          <Menu.Item>
               中文
           </Menu.Item>
-          <Menu.Item  onClick={()=>props.changeLocal(props.locale==="zh"?"en":"zh")}>
+          <Menu.Item>
               英文
           </Menu.Item>
         </Menu>
